35e362d231a11cf230f48e638d5787b83a8ddfaa
Bouken Shounen <kidd.dawny.lu@gmail.com>
2017-08-29 19:21:58 -0700

    update
    Signed-off-by: Bouken Shounen <kidd.dawny.lu@gmail.com>

---
+++ unix/dirname.c
@@ -0,0 +1,28 @@
+#include "libgen.h"
+#include "string.h"
+
+char *dirname(char *path)
+{
+    char *p;
+
+    if( path == NULL || *path == '\0')
+        return ".";
+
+    p = path + strlen(path) - 1;
+    while( *p == '/' ) {
+        if( p == path )
+            return path;
+        *p-- = '\0';
+    }
+    while ( p >= path && *p != '/' )
+        p--;
+    
+    if( p < path )
+        return ".";
+    else if ( p == path )
+        return "/"
+    else {
+        *p = '\0';
+        return path;
+    }
+}
