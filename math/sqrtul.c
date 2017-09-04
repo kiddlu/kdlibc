bb1972c28379624c64e9a426eeaee82c60ebf998
kiddlu <kidd.dawny.lu@gmail.com>
2017-09-04 05:53:19 -0700

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

---
+++ math/sqrtul.c
@@ -0,0 +1,26 @@
+/*
+ copy from linux/lib/int_sqrt.c 
+*/
+
+unsigned long sqrtul(unsigned long x)
+{
+	unsigned long b, m, y = 0;
+
+	if (x <= 1)
+		return x;
+
+	m = 1UL << (BITS_PER_LONG - 2);
+	while (m != 0) {
+		b = y + m;
+		y >>= 1;
+
+		if (x >= b) {
+			x -= b;
+			y += m;
+		}
+		m >>= 2;
+	}
+
+	return y;
+}
+
